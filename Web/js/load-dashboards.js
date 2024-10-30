1620fd847b1ca66f025a77c17de233aede84697f
kiukhanh <vothikieukhanh@gmail.com>
2024-10-31 02:02:24 +0700

    add Web

---
+++ Web/js/load-dashboards.js
@@ -0,0 +1,9 @@
+// center dashboard
+$("#world-gdp").load("./assets/dashboards/world-gdp.html");
+
+// detail elements
+$("#gdp-vietnam-overview").load("./assets/dashboards/gdp-vietnam-overview.html");
+$("#population-statistic").load("./assets/dashboards/population-statistic.html");
+$("#labor-force").load("./assets/dashboards/labor-force.html");
+$("#indexes").load("./assets/dashboards/indexes.html");
+$("#three-growthest-year").load("./assets/dashboards/three-growthest-year.html");
